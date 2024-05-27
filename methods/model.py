6704186cbad236282419b19275ace6da6f87af5c
Le Duc Minh <minhmeo7a1@gmail.com>
2024-05-28 00:24:58 +0700

    Revert old code

--- methods/model.py
+++ methods/model.py
@@ -9,10 +9,9 @@ class Classifier(nn.Module):
         super().__init__()
         self.head = nn.Sequential(
             nn.Linear(args.encoder_output_size * 2, args.encoder_output_size, bias=True),
-            # nn.ReLU(inplace=True),
-            nn.GELU(),
+            nn.ReLU(inplace=True),
             nn.Linear(args.encoder_output_size, args.encoder_output_size, bias=True),
-            # nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Linear(args.encoder_output_size, args.rel_per_task * args.num_tasks),
         ).to(args.device)
 
