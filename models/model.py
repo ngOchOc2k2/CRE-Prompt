95cc2ff4b32206e352b253d17737abe3491ec5dd
Le Duc Minh <minhmeo7a1@gmail.com>
2024-05-28 12:19:48 +0700

    Revert old code

--- models/model.py
+++ models/model.py
@@ -1,25 +1,4 @@
 import torch.nn as nn
-import torch
-import torch.nn.functional as F
-from .backbone import BertRelationEncoder
-
-
-# class Classifier(nn.Module):
-#     def __init__(self, args):
-#         super().__init__()
-#         self.mlp = nn.Sequential(
-#             nn.Linear(args.encoder_output_size * 2, args.encoder_output_size),
-#             nn.GELU(),
-#             nn.Linear(args.encoder_output_size, args.encoder_output_size * 2),
-#         ).to(args.device)
-
-#         self.head =  nn.Linear(args.encoder_output_size * 2, args.num_of_relation).to(args.device)
-
-#     def forward(self, x):
-#         out = self.mlp(x)
-#         out = out + x
-#         return self.head(out)
-
 
 
 class Classifier(nn.Module):
@@ -27,17 +6,16 @@ class Classifier(nn.Module):
         super().__init__()
         self.mlp = nn.Sequential(
             nn.Linear(args.encoder_output_size * 2, args.encoder_output_size),
-            nn.ReLU(),
-            nn.Linear(args.encoder_output_size, args.encoder_output_size),
-            nn.ReLU(),
+            nn.GELU(),
+            nn.Linear(args.encoder_output_size, args.encoder_output_size * 2),
         ).to(args.device)
 
-        self.head =  nn.Linear(args.encoder_output_size, args.num_of_relation).to(args.device)
+        self.head =  nn.Linear(args.encoder_output_size * 2, args.num_of_relation).to(args.device)
 
     def forward(self, x):
         out = self.mlp(x)
+        out = out + x
         return self.head(out)
-
 
 
 class ClassifierBasic(nn.Module):
